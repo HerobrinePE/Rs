464bf9bd42b51ecafe53cf27d4fd692fc3109337
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-11-03 14:38:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const { Client, Collection, RichEmbed } = require("discord.js");
 const fs = require("fs");
-let prefix= "+"
+let pre= "!"
 require("events").EventEmitter.defaultMaxListeners = 100;
 const client = new Client({
   disableEveryone: true
 });
 client.login(process.env.TOKEN);
+const { Client, Collection, RichEmbed } = require("discord.js");
+const fs = require("fs");
+require("events").EventEmitter.defaultMaxListeners = 100;
+const client = new Client({
+  disableEveryone: true
+});
+client.login(process.env.TOKEN);
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+
+["command"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
+});
+client.on("message", async message => {
+  const prefix = pre;
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+  if (cmd.length === 0) return;
+  let command = client.commands.get(cmd);
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+  if (command) command.run(client, message, args);
+});
